dc7cff5a8b6f3c4cb290b0943d0de1c2b1776ba7
Medcheikhna <mohamedsidili77@gmail.com>
2023-12-23 12:35:47 +0000

    first Project Spring boot

--- src/main/java/tech/chilo/sa/SaBackendApplication.java
+++ src/main/java/tech/chilo/sa/SaBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "tech.chilo.sa")
+
 public class SaBackendApplication {
 
 	public static void main(String[] args) {
